91fa07618386b1edceb7619db6adbda487975fc7
whateveritwas <whateveritwars@web.de>
2025-04-26 16:18:24 +0200

    Started implementing audio player

--- src/menu.hpp
+++ src/menu.hpp
@@ -14,7 +14,8 @@ void ui_init(SDL_Window* _window, SDL_Renderer* _renderer, SDL_Texture* &_textur
 void ui_render();
 void ui_render_settings();
 void ui_render_file_browser();
-void ui_render_video();
+void ui_render_video_player();
+void ui_render_audio_player();
 void ui_render_console();
 void ui_shutodwn();
 
