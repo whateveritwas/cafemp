0ffde0f00247915be32171fb0a57d4b02fa5fce2
whateveritwas <whateveritwars@web.de>
2025-05-20 09:04:11 +0200

    Hud and video player use media_info the store and get data about current playing media

---
+++ src/media_info.cpp
@@ -0,0 +1,15 @@
+#include <mutex>
+#include "media_info.hpp"
+
+static media_info current_media_info;
+static std::mutex media_info_mutex;
+
+media_info media_info_get() {
+    std::lock_guard<std::mutex> lock(media_info_mutex);
+    return current_media_info;
+}
+
+void media_info_set(media_info& new_media_info) {
+    std::lock_guard<std::mutex> lock(media_info_mutex);
+    current_media_info = new_media_info;
+}
