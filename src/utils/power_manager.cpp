5567346e5ced6ff9cbae7ccf529919d7eff5646a
whateveritwas <whateveritwars@web.de>
2025-12-10 21:26:36 +0100

    Power manager refactor

--- src/utils/power_manager.cpp
+++ src/utils/power_manager.cpp
@@ -4,30 +4,48 @@
 
 #include "utils/power_manager.hpp"
 
+uint32_t power_manager_is_auto_power_down_enabled = 0;
+uint32_t power_manager_is_auto_dim_enabled = 0;
+
 int power_manager_sleep_enable(bool state) {
-	IMError error;
+    IMError error = 0;
 
-	if (state) {
-		error = IMDisableAPD();
-		if (error != 0) goto error;
+    if ((error = IMIsAPDEnabled(&power_manager_is_auto_power_down_enabled)) != 0)
+        goto error;
 
-		error = IMDisableDim();
+    if ((error = IMIsDimEnabled(&power_manager_is_auto_dim_enabled)) != 0)
+        goto error;
 
-		log_message(LOG_OK, "Power Manager", "Enabled power managment");
-	} else {
-		error = IMDisableAPD();
-		if (error != 0) goto error;
+#ifdef DEBUG
+    log_message(LOG_DEBUG, "Power Manager", "Restore state is apd: %s, dim: %s",
+							power_manager_is_auto_power_down_enabled ? "on" : "off", power_manager_is_auto_dim_enabled ? "on" : "off");
+#endif
 
-		error = IMDisableDim();
+    if (state) {
+        if (power_manager_is_auto_power_down_enabled &&
+            (error = IMEnableAPD()) != 0)
+            goto error;
 
-		log_message(LOG_OK, "Power Manager", "Disabled power managment");
-	}
+        if (power_manager_is_auto_dim_enabled &&
+            (error = IMEnableDim()) != 0)
+            goto error;
 
-	if (error != 0) goto error;
+        log_message(LOG_OK, "Power Manager", "Enabled power management");
+    } else {
+        if (power_manager_is_auto_power_down_enabled &&
+            (error = IMDisableAPD()) != 0)
+            goto error;
 
-	return 0;
+        if (power_manager_is_auto_dim_enabled &&
+            (error = IMDisableDim()) != 0)
+            goto error;
+
+        log_message(LOG_OK, "Power Manager", "Disabled power management");
+    }
+
+    return 0;
 
 error:
-	log_message(LOG_WARNING, "Power Manager", "Faild power managment with code %i", error);
-	return error;
+    log_message(LOG_WARNING, "Power Manager", "Failed power management with code %i", error);
+    return error;
 }
